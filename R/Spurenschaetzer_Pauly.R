7322d7058afb65d615b13fb7314679ed0eb95b3c
Schnieboli <119417363+Schnieboli@users.noreply.github.com>
2024-06-28 17:53:49 +0200

    Add files via upload

---
+++ R/Spurenschaetzer_Pauly.R
@@ -0,0 +1,36 @@
+# Jeweils eine Matrix eingeben
+
+B0 <- function(X, N){
+  S <- 0
+  for (i in 1:N) {
+    S <- S + sum(X[i, ]^2)
+  }
+  return(S/N)
+}
+
+
+
+B2 <- function(X, N){
+  S <- 0
+  for (k in 1:N) {
+    for(l in 1:N){
+      if(k != l) S <- S + (sum(X[k, ] * X[l, ])^2)
+    }
+  }
+  return(S / (N*(N-1)))
+}
+
+
+
+B3 <- function(X, N){
+  S <- 0
+  #browser()
+  for (k in 1:(N-2)) {
+    for (l in (k+1):(N-1)) {
+      for (r in (l+1):N) {
+        S <- S + (sum(X[k, ] * X[l, ]) * sum(X[l, ] * X[r, ]) * sum(X[r, ] * X[k, ]))
+      }
+    }
+  }
+  return(S/choose(N,3))
+}
