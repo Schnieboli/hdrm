bd48c4b82daf61f2e44a2057d14c79e95b503731
Schnieboli <119417363+Schnieboli@users.noreply.github.com>
2024-08-04 00:15:17 +0200

    reintroduced hdrm_test
    changed the generics so it works

--- R/Spurenschaetzer_Sattler.R
+++ R/Spurenschaetzer_Sattler.R
@@ -4,18 +4,16 @@
 
 
 #' @keywords internal
-A1 <- function(X, n){# braucht Individuen in Zeilen und dimension in Spalten
+A1 <- function(X){# braucht Individuen in Spalten und dimension in Zeilen
   Result <- 0
-  n <- dim(X)[1]
-  for (k in 1:(n - 1))
-  {
-    for (l in (k + 1):n)
-    {
-      a1 <- (X[l, ] - X[k, ])
-      Result <- Result + sum(a1 * a1)
+  n <- ncol(X)
+  for (i in 1:(n - 1)){
+    for (j in (i + 1):n){
+      a1 <- X[, i] - X[, j]
+      Result <- Result + sum(a1*a1)
     }
   }
-  return(Result / (2 * choose(n, 2)))
+  return(Result)
 }
 
 # Idee: ziehe 6 zufällige Vektoren aus jeder Gruppe
@@ -30,19 +28,17 @@ A1_star <- function(B){
 
 #' @keywords internal
 A2 <- function(X, Y){# braucht Individuen in Zeilen und dimension in Spalten
-  nX <- dim(X)[1] # Anzahl Individuen in Gruppe
-  nY <- dim(Y)[1]
+  nX <- ncol(X) # Anzahl Individuen in Gruppe
+  nY <- ncol(Y)
   PX <- diag(1, nX, nX) - matrix(data = 1 / nX, # Zentrierungsmatrix für Gruppe X
                                  nrow = nX,
                                  ncol = nX)
   PY <- diag(1, nY, nY) - matrix(data = 1 / nY, # Zentrierungsmatrix für Gruppe Y
                                  nrow = nY,
                                  ncol = nY)
-  MXY <- PX %*% (X) %*% t(Y) %*% t(PY) # Seite 38 im Paper
-  EBSchaetzer = matrix(data = 1, nrow = 1, ncol = nX) %*%
-    (MXY * MXY) %*% matrix(data = 1, nrow = nY, ncol = 1) / ((nX - 1) * (nY - 1))
-  # (Edgar Brunner Schätzer)
-  return(as.vector(EBSchaetzer))
+  MXY <- PX %*% t(X) %*% Y %*% PY # Seite 38 im Paper
+  EBSchaetzer = sum(MXY^2)
+  return(EBSchaetzer)
 }
 
 
@@ -87,25 +83,25 @@ A2 <- function(X, Y){# braucht Individuen in Zeilen und dimension in Spalten
 #   return(S / (24*choose(nX,4)))
 # }
 #
-# A3_schlecht <- function(X){
-#   nX <- dim(X)[1]
-#   S <- 0
-#   comb <- combn(nX, 2)
-#
-#   for (i in 1:choose(nX,2)) {
-#     l1 <- comb[1,i]
-#     l2 <- comb[2,i]
-#     for(k2 in 1:(nX-1)){
-#       for(k1 in k2:nX){
-#         if(k2 != l1 & k2 != l2 & k1 != l1 & k1 != l2) S <- S + sum((X[l1, ] - X[l2, ]) * (X[k1, ] - X[k2, ]))^2
-#       }
-#     }
-#   }
-#   return(S / (24 * choose(nX,4)))
-# }
+A3_schlecht <- function(X){
+  nX <- ncol(X)
+  S <- 0
+  comb <- combn(nX, 2)
+
+  for (l1 in 1:(nX-1)) {
+    for(l2 in (l1+1):nX){
+      for(k2 in 1:(nX-1)){
+        for(k1 in (k2+1):nX){
+          if(k2 != l1 & k2 != l2 & k1 != l1 & k1 != l2) S <- S + sum((X[, l1] - X[, l2]) * (X[, k1] - X[, k2]))^2
+        }
+      }
+    }
+  }
+  return(S / (24 * choose(nX,4)))
+}
 
 #' @keywords internal
-A3 <- function(X){ # braucht Individuen in Zeilen und dimension in Spalten
+A3 <- function(X){
   nX = dim(X)[1]
   Part1 = 0
   Part2 = 0
@@ -115,17 +111,15 @@ A3 <- function(X){ # braucht Individuen in Zeilen und dimension in Spalten
   Part6 = 0
   Part7 = 0
 
-  # browser()
-
-  for (l2 in 1:(nX)){
-    a22 = t(X[l2, ]) %*% X[l2, ]
+  for (l2 in 1:nX){
+    a22 = sum(X[l2,]^2)
     Part7 = Part7 + a22
     for (l1 in 1:nX){
-      a12 = (t(X[l1, ]) %*% X[l2, ])
-      Part1 = Part1 + (a12) ^ 2 * (l1 != l2)
+      a12 = sum(X[l1, ] * X[l2, ])
+      Part1 = Part1 + (a12)^2 * (l1 != l2)
       for (l3 in 1:nX){
-        a23 = (t(X[l2, ]) %*% X[l3, ])
-        a13 = (t(X[l1, ]) %*% X[l3, ])
+        a23 = sum(X[l2, ] * X[l3, ])
+        a13 = sum(X[l1, ] * X[l3, ])
         Part5 = Part5 + a12 * a23 * (l1 != l2)
         Part2 = Part2 + a12 * a13 * (l1 != l2) * (l1 != l3) * (l2 != l3)
         Part3 = Part3 + a13 * (a23 + a12) * (l1 != l3) * (l2 != l3)
@@ -133,13 +127,14 @@ A3 <- function(X){ # braucht Individuen in Zeilen und dimension in Spalten
       }
     }
   }
+
   Part1 = Part1 * (nX - 2) * (nX - 3)
   Part2 = Part2 * (2 * nX - 5)
   a8 = colMeans(X)
   Part6 = nX ^ 2 * sum(a8 * a8)
 
   PSSchaetzer = (Part1 - Part2 - Part3 - Part4 - Part5 + Part6 * (Part6 - Part7)) / (nX * (nX - 1) * (nX - 2) * (nX - 3))
-  return(PSSchaetzer)
+  return(as.numeric(PSSchaetzer))
 }
 
 
