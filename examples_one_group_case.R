987e7d5484ab9c52ccfb27789b5b0e7f94c84620
Schnieboli <119417363+Schnieboli@users.noreply.github.com>
2024-08-08 19:30:54 +0200

    one group case is done (for now)

--- examples_one_group_case.R
+++ examples_one_group_case.R
@@ -6,13 +6,12 @@ M <- matrix(rnorm(d*N), d, N)
 
 # test M for H_0 = mu_1 = mu_2 = mu_3 = 0 time profile
 H <- matrix(0,d,d)
-H[c(1,d+2,2*d+3)] <- 1
+H[c(1, d+2, 2*d+3)] <- 1
 hdrm1(data = M, hypothesis = H)
 
 ## hdrm1 with data.frame in longtable format
 head(EEG)
-# for 'formula' we are interested in value = value, subject = subject and
-# factor = dimension
+# for 'formula' we are interested in value = value, subject = subject and factor = dimension
 formula <- value ~ subject + dimension
 
 # test EEG for equal time profile
