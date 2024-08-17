d480b708b9cd079c968db83e5a6161b029d091e7
Schnieboli <119417363+Schnieboli@users.noreply.github.com>
2024-08-17 12:44:32 +0200

    added and edited some becnhmakrs

--- Benchmarks/Benchmarks_Sattler.R
+++ Benchmarks/Benchmarks_Sattler.R
@@ -15,13 +15,44 @@ M2 <- matrix(rnorm(N*d[2]), d[2], N)
 M3 <- matrix(rnorm(N*d[3]), d[3], N)
 M4 <- matrix(rnorm(N*d[4]), d[4], N)
 M5 <- matrix(rnorm(N*d[5]), d[5], N)
+M6 <- matrix(rnorm(100*d[5]), d[5], 100)
 
 
+# A1 ----------------------------------------------------------------------
 
-# A2 ----------------------------------------------------------------------
+### wenn man sehr große Matrizen betrachtet, dann kommt R bis auf 14x an cpp ran -> das muss auch auf jeden Fall in die Arbeit
+
+# d < N
+microbenchmark(R = A1(M1),
+               cpp = A1_cpp(M1),
+               unit = "relative",
+               check = "equal")
+
+# d = N
+microbenchmark(R = A1(M2),
+               cpp = A1_cpp(M2),
+               unit = "relative",
+               check = "equal")
+
+# d > N
+microbenchmark(R = A1(M3),
+               cpp = A1_cpp(M3),
+               unit = "relative",
+               check = "equal")
 
 
+# d >> N
+microbenchmark(R = A1(M4),
+               cpp = A1_cpp(M4),
+               unit = "relative",
+               check = "equal")
 
+
+# d >>> N
+microbenchmark(R = A1(M5),
+               cpp = A1_cpp(M5),
+               unit = "relative",
+               check = "equal")
 
 
 # A3 ----------------------------------------------------------------------
@@ -43,4 +74,35 @@ A3_intuitiv <- function(X){
   return(S / (24 * choose(nX,4)))
 }
 
+
+microbenchmark(R = A3(M6),
+               cpp = A3_cpp(M6, sum(rowMeans(M6)^2)),
+               unit = "relative",
+               check = "equal",
+               times = 5)
+
+
+# A2 ----------------------------------------------------------------------
+
+## quasi gleich schnell
+
+n1 <- 580
+n2 <- 200
+d <- 365
+M1 <- matrix(rnorm(n1*d),d,n1)
+M2 <- matrix(rnorm(n2*d),d,n2)
+# P1 <- diag(n1) - matrix(1/n1,n1,n1)
+# P2 <- diag(n2) - matrix(1/n2,n2,n2)
+# PM1 <- P1%*%t(M1)
+# PM2 <- M2 %*% P2
+
+microbenchmark(R = A2(M1,M2),
+               cpp = {
+                 P1 <- diag(n1) - matrix(1/n1,n1,n1)
+                 P2 <- diag(n2) - matrix(1/n2,n2,n2)
+                 sum(A2_cpp(M1, M2, P1, P2))/((n1-1)*(n2-1))
+                 },
+               unit = "relative",
+               check = "equal"
+               )
 
