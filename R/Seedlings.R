bb38a002dc28f58ac5b12f4980b42889f7f9e8a6
Schnieboli <119417363+Schnieboli@users.noreply.github.com>
2024-08-17 21:39:17 +0200

    added dataset Seedling
    maybe include this data

---
+++ R/Seedlings.R
@@ -0,0 +1,9 @@
+#' Seedlings
+#'
+#' Repeated Measures of
+#'
+#'
+#' @source Nemec, A F. Analysis of repeated measures and times series: An introduction with forestry examples. Biometrics information handbook No. 6, and Working paper No. 15/1996. Canada.
+#'
+
+"Seedlings"
