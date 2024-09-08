6f75951d5554f00c87c9192f082a42a225bf07a5
Schnieboli <119417363+Schnieboli@users.noreply.github.com>
2024-09-08 14:55:22 +0200

    data aktualisiert und dokumentiert
    dokumentation mit Rdpack

--- R/EEG.R
+++ R/EEG.R
@@ -1,9 +1,24 @@
-#' EEG data
+#' EEG data of 160 subjects
 #'
-#' EEG data from...
+#' A dataset containing EEG data \insertCite{EEG_dataset}{hdrm} of 160 subjects, 4 variables are measured at ten different locations.
 #'
-#' @references \insertRef{EEG_dataset}{hdrm}
+#' \itemize{
+#'   \item group: Diagnostic group of the subject: Alzheimer's Disease (AD), Mild Cognitive Impairment (MCI), Subject Cognitive Complaints (SCC+, SCC-).
+#'   \item value: Measured data of a subject at a specific variable and region.
+#'   \item sex: Sex of the subject: Male (M) or Female (W).
+#'   \item subject: A unique identification of a subject.
+#'   \item variable: The variables measured are activity, complexity, mobility and brain rate coded from 1 to 4.
+#'   \item region: Frontal left/right, central left/right, temporal left/right, occipital left/right, parietal left/right coded as 1 to 10.
+#'   \item dimension: Mixing variable and region together, levels range from 1 to 40.
+#' }
+#' Documentation taken from \insertCite{HRM_RJournal;textual}{hdrm}.
+#' @docType data
+#' @keywords datasets
+#' @name EEG
+#' @usage data(EEG)
+#' @format A data frame with 6400 rows and 7 variables.
+#' @references {
+#'  \insertAllCited{}
+#' }
 #'
-
 "EEG"
-
---
+++ R/sunspots_matrix.R
@@ -0,0 +1,14 @@
+#' Monthly Sunspot Numbers, 1749-1983
+#'
+#' @description Monthly mean relative sunspot numbers from 1749 to 1983 \insertCite{sunspots_dataset}{hdrm}. Collected at Swiss Federal Observatory, Zurich until 1960, then Tokyo Astronomical Observatory.
+#' The object `sunspots_matrix` is the object [sunspots] from the package [datasets] converted to a matrix, to make it readily available for the methods in this package.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name sunspots_matrix
+#' @usage data(sunspots_matrix)
+#'
+#' @references \insertAllCited
+#'
+
+"sunspots_matrix"
