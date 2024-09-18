e49c34bb6030119fab585f31cee51eab0d13d966
Schnieboli <119417363+Schnieboli@users.noreply.github.com>
2024-09-18 16:40:50 +0200

    added dataset

---
+++ R/birthrates.R
@@ -0,0 +1,6 @@
+#' Yearly birthrates in German States 1990-2023
+#'
+#'
+#' @references Statistisches Bundesamt (Destatis), Genesis-Online, \href{https://www-genesis.destatis.de/genesis//online?operation=table&code=12612-0012&bypass=true&levelindex=0&levelid=1726659625856#abreadcrumb}{Table 12612-0009}. Last visit 18.09.2024; \href{www.govdata.de/dl-de/by-2-0}{Datenlizenz by-2-0}.
+
+"birthrates"
