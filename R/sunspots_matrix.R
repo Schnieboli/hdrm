cc68c56d9649931fca5544e74e8de090b8b660d7
Schnieboli <119417363+Schnieboli@users.noreply.github.com>
2024-09-18 16:40:22 +0200

    deleted dataset sunspots_matrix

--- R/sunspots_matrix.R
+++
@@ -1,9 +0,0 @@
-#' Monthly Sunspot Numbers, 1749-1983
-#'
-#' @description Monthly mean relative sunspot numbers from 1749 to 1983 \insertCite{sunspots_dataset}{hdrm}. Collected at Swiss Federal Observatory, Zurich until 1960, then Tokyo Astronomical Observatory.
-#' The object `sunspots_matrix` is the object [sunspots] from the package [datasets] converted to a matrix, to make it readily available for the methods in this package.
-#'
-#' @references \insertAllCited
-#'
-
-"sunspots_matrix"
