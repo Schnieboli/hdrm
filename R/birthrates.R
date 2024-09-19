8e0c79b321712e9123d92f1fdbe1b1bd054e0c78
Schnieboli <119417363+Schnieboli@users.noreply.github.com>
2024-09-19 20:15:29 +0200

    changed documentation for birthrates

--- R/birthrates.R
+++ R/birthrates.R
@@ -1,8 +1,7 @@
 #' Yearly birthrates in German States 1990-2023
 #'
-#' A dataset containing the number of births per woman in each German State for 1990-2023.
+#' A dataset containing the number of births per woman in each German State for 1990-2023 \insertCite{birthrates}{hdrm}.
 #'
-#'
-#' @references Statistisches Bundesamt (Destatis). (2023). Statistischer Bericht - Geburten, Tabelle 12612-09. Statisitsches Bundesamt.
+#' @references {\insertAllCited{}}
 
 "birthrates"
