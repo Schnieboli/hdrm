7beb7afebaf763c7404d7bbd1233a92756f1a01c
Schnieboli <119417363+Schnieboli@users.noreply.github.com>
2024-09-23 16:37:01 +0200

    update documentation
    update documentation birthrates

--- R/birthrates.R
+++ R/birthrates.R
@@ -1,6 +1,8 @@
 #' Yearly birthrates in German States 1990-2023
 #'
-#' A dataset containing the number of births per woman in each German State for 1990-2023 \insertCite{birthrates}{hdrm}.
+#' A data set containing the number of births per woman in each German State for
+#' 1990-2023 \insertCite{birthrates}{hdrm}. States are represented by columns
+#' and time is represented by rows.
 #'
 #' @references {\insertAllCited{}}
 
