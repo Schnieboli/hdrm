4926a82b2d0ef1a5ee5973804f39a9a5780bf31e
Schnieboli <119417363+Schnieboli@users.noreply.github.com>
2024-11-15 11:46:22 +0100

    added cpp estimator A1_eq
    added cpp estimator A1_eq

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -11,6 +11,17 @@ Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
 Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
 #endif
 
+// A1_eq_cpp
+double A1_eq_cpp(arma::mat& mat);
+RcppExport SEXP _hdrm_A1_eq_cpp(SEXP matSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< arma::mat& >::type mat(matSEXP);
+    rcpp_result_gen = Rcpp::wrap(A1_eq_cpp(mat));
+    return rcpp_result_gen;
+END_RCPP
+}
 // A1_cpp
 double A1_cpp(arma::mat& mat);
 RcppExport SEXP _hdrm_A1_cpp(SEXP matSEXP) {
@@ -120,6 +131,7 @@ END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
+    {"_hdrm_A1_eq_cpp", (DL_FUNC) &_hdrm_A1_eq_cpp, 1},
     {"_hdrm_A1_cpp", (DL_FUNC) &_hdrm_A1_cpp, 1},
     {"_hdrm_A3_cpp", (DL_FUNC) &_hdrm_A3_cpp, 2},
     {"_hdrm_A1star_cpp", (DL_FUNC) &_hdrm_A1star_cpp, 2},
---
+++ src/equal_covariances_utils.cpp
@@ -0,0 +1,24 @@
+#include <RcppArmadillo.h>
+using namespace Rcpp;
+
+
+// [[Rcpp::export()]]
+double A1_eq_cpp(arma::mat &mat){
+  int d = mat.n_rows;
+  int n = mat.n_cols;
+  double tmp = 0.0;
+  double out = 0.0;
+  arma::vec col1(d), col2(d);
+
+  for(int l2 = 0; l2 < (n-1); ++l2){
+    col1 = mat.col(l2);
+    for(int l1 = l2; l1 < n; ++l1){
+      col2 = mat.col(l1);
+      for(int j = 0; j < d; ++j){
+        tmp = col2(j) - col1(j);
+        out += pow(tmp, 2);
+      }
+    }
+  }
+  return out;
+}
