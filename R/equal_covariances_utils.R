2e861e3266170a035a5d81ea1db78da6938a397a
Schnieboli <119417363+Schnieboli@users.noreply.github.com>
2024-11-15 14:36:27 +0100

    added C1_eq and C1_star_eq
    added C1_eq and C1_star_eq; results of C5_star_eq arent plausible :(

--- R/equal_covariances_utils.R
+++ R/equal_covariances_utils.R
@@ -43,3 +43,52 @@ A2_eq <- function(X){
 
   return(as.numeric(S))
 }
+
+
+## C1_eq - function that calculates the estimator C1 form the equal covariances paper.
+##
+## Input: a numeric matrix, where cols represent the individuals of one group
+## and rows represent dimensions
+##
+## Output: a numeric vector that gives the group estimation
+C1_eq <- function(X){
+  n <- ncol(X)
+  S <- 0.0
+  for(l1 in 1:(n-5)){
+    for(l2 in (l1+1):(n-4)){
+      for(l3 in (l2+1):(n-3)){
+        for(l4 in (l3+1):(n-2)){
+          for(l5 in (l4+1):(n-1)){
+            for(l6 in (l5+1):(n)){
+              S <- S + crossprod(X[, l1] - X[, l2], X[, l3] - X[, l4]) *
+                crossprod(X[, l3] - X[, l4], X[, l5] - X[, l6]) *
+                crossprod(X[, l5] - X[, l6], X[, l1] - X[, l2])
+            }
+          }
+        }
+      }
+    }
+  }
+  return(as.numeric(S))
+}
+
+
+## C1_star_eq - function that calculates the estimator C1* form the equal covariances paper.
+##
+## Input:
+##      X - a numeric matrix, where cols represent the individuals of one group
+##          and rows represent dimensions
+##      B - an integer, determines the number of subsamplings
+##
+## Output: a numeric vector that gives the group estimation
+C1_star_eq <- function(X, B){
+  n <- ncol(X)
+  S <- 0.0
+  for (i in 1:B) {
+    b <- sample.int(n = n, size = 6, replace = FALSE)
+    S <- S + crossprod(X[, b[1]] - X[, b[2]], X[, b[3]] - X[, b[4]]) *
+      crossprod(X[, b[3]] - X[, b[4]], X[, b[5]] - X[, b[6]]) *
+      crossprod(X[, b[5]] - X[, b[6]], X[, b[1]] - X[, b[2]])
+  }
+  return(as.numeric(S))
+}
