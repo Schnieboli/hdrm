53cc9a1c3ed95155f9216e456cd1869e1c4c73e2
Schnieboli <119417363+Schnieboli@users.noreply.github.com>
2024-11-15 10:59:01 +0100

    Update equal_covariances_utils.R
    documentation added to A2_eq

--- R/equal_covariances_utils.R
+++ R/equal_covariances_utils.R
@@ -9,15 +9,22 @@ A1_eq <- function(X){
   return(as.numeric(S)/(n*(n-1)))
 }
 
-
+## A2_eq - function that calculates the estimator A2 form the equal covariances paper.
+##
+## Input: a numeric matrix, where cols represent the individuals of one group
+## and rows represent dimensions
+##
+## Output: a numeric vector that gives the group estimation
 A2_eq <- function(X){
   n <- nrow(X)
   S <- 0.0
   for (l2 in 1:(n-1)) {
     for(l1 in l2:n){
       for(k2 in 1:(n-1)){
+        # only call the loop if this condition is true
         if(k2 != l1 && k2!= l2){
           for(k1 in k2:n){
+        # only add to total if this condition is true
             if(k1 != l1 && k1 != l2){
               S <- S + (crossprod(X[l1, ] - X[l2, ], X[k1, ] - X[k2, ]))^2
             }
@@ -26,5 +33,6 @@ A2_eq <- function(X){
       }
     }
   }
-  return(as.numeric(S)/(24*choose(n, 4)))
+
+  return(as.numeric(S))
 }
