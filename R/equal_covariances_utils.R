87719b2aaeb8b6b51bfc5b562036ed9b593a3756
Schnieboli <119417363+Schnieboli@users.noreply.github.com>
2024-11-15 13:17:49 +0100

    Update equal_covariances_utils.R
    start of loops corrected

--- R/equal_covariances_utils.R
+++ R/equal_covariances_utils.R
@@ -9,7 +9,7 @@ A1_eq <- function(X){
   n <- ncol(X)
   S <- 0.0
   for(l2 in 1:(n-1)){
-    for(l1 in l2:n){
+    for(l1 in (l2+1):n){
       S <- S + sum((X[, l1] -  X[, l2])^2)
     }
   }
@@ -26,11 +26,11 @@ A2_eq <- function(X){
   n <- ncol(X)
   S <- 0.0
   for (l2 in 1:(n-1)) {
-    for(l1 in l2:n){
+    for(l1 in (l+1)2:n){
       for(k2 in 1:(n-1)){
         # only call the loop if this condition is true
         if(k2 != l1 && k2!= l2){
-          for(k1 in k2:n){
+          for(k1 in (k2+1):n){
         # only add to total if this condition is true
             if(k1 != l1 && k1 != l2){
               S <- S + (crossprod(X[, l1] - X[, l2], X[, k1] - X[, k2]))^2
