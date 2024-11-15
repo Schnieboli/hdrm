8706addbbeac010011cb06d9fc952130fd5d8121
Schnieboli <119417363+Schnieboli@users.noreply.github.com>
2024-11-15 15:10:57 +0100

    added utils
    added functions, that calculate the estimators for all groups

--- R/equal_covariances_utils.R
+++ R/equal_covariances_utils.R
@@ -4,7 +4,7 @@
 ## Input: a numeric matrix, where cols represent the individuals of one group
 ## and rows represent dimensions
 ##
-## Output: a numeric vector that gives the group estimation
+## Output: a numeric vector that gives the group estimation for A1
 A1_eq <- function(X){
   n <- ncol(X)
   S <- 0.0
@@ -16,12 +16,34 @@ A1_eq <- function(X){
   return(S)
 }
 
+## make_A1_eq - function that combines the calculators A1_eq for all groups.
+##
+## Input:
+##      X - a numeric matrix, where cols represent the individuals of one group
+##          and rows represent dimensions
+##  group - an integer vector, specifies the group membership
+##
+## Output: numeric; gives the estimator A1 for equal covariances
+make_A1_eq <- function(X, group){
+  ns <- unname(table(group))
+  a <- length(ns)
+  out <- 0.0
+  prefactor <- 0
+  for(i in 1:a){
+    n <- sum
+    out <- out + A1_eq(X[, group == i])
+    prefactor <- prefactor + (ns[i]* (ns[i] - 1))
+  }
+  return(out/(prefactor))
+}
+
+
 ## A2_eq - function that calculates the estimator A2 form the equal covariances paper.
 ##
 ## Input: a numeric matrix, where cols represent the individuals of one group
 ## and rows represent dimensions
 ##
-## Output: a numeric vector that gives the group estimation
+## Output: a numeric vector that gives the group estimation for A2
 A2_eq <- function(X){
   n <- ncol(X)
   S <- 0.0
@@ -44,11 +66,33 @@ A2_eq <- function(X){
   return(as.numeric(S))
 }
 
+## make_A2_eq - function that combines the calculators A2_eq for all groups.
+##
+## Input:
+##      X - a numeric matrix, where cols represent the individuals of one group
+##          and rows represent dimensions
+##  group - an integer vector, specifies the group membership
+##
+## Output: numeric; gives the estimator A2 for equal covariances
+make_A2_eq <- function(X, group){
+  ns <- unname(table(group))
+  a <- length(ns)
+  out <- 0.0
+  prefactor <- 0
+  for(i in 1:a){
+    n <- sum
+    out <- out + A2_eq(X[, group == i])
+    prefactor <- prefactor + (24* choose(ns[i], 4))
+  }
+  return(out/(prefactor))
+}
+
 
 ## C1_eq - function that calculates the estimator C1 form the equal covariances paper.
 ##
-## Input: a numeric matrix, where cols represent the individuals of one group
-## and rows represent dimensions
+## Input:
+##      X - a numeric matrix, where cols represent the individuals of one group
+##          and rows represent dimensions
 ##
 ## Output: a numeric vector that gives the group estimation
 C1_eq <- function(X){
@@ -72,6 +116,26 @@ C1_eq <- function(X){
   return(as.numeric(S))
 }
 
+## make_C1_eq - function that combines the calculations C1_eq for all groups.
+##
+## Input:
+##      X - a numeric matrix, where cols represent the individuals of one group
+##          and rows represent dimensions
+##  group - an integer vector, specifies the group membership
+##
+## Output: numeric; gives the estimator A2 for equal covariances
+make_C1_eq <- function(X, group){
+  ns <- unname(table(group))
+  a <- length(ns)
+  out <- 0.0
+  prefactor <- 0
+  for(i in 1:a){
+    n <- sum
+    out <- out + C1_eq(X[, group == i])
+    prefactor <- prefactor + (choose(ns[i], 6))
+  }
+  return(out/(prefactor * 5760)) ## 5760 = 6! * 8
+}
 
 ## C1_star_eq - function that calculates the estimator C1* form the equal covariances paper.
 ##
@@ -91,4 +155,25 @@ C1_star_eq <- function(X, B){
       crossprod(X[, b[5]] - X[, b[6]], X[, b[1]] - X[, b[2]])
   }
   return(as.numeric(S))
+}
+
+## make_C1_star_eq - function that combines the calculations C1_star_eq for all groups.
+##
+## Input:
+##      X - a numeric matrix, where cols represent the individuals of one group
+##          and rows represent dimensions
+##      B - an integer, determines the number of subsamplings
+##  group - an integer vector, specifies the group membership
+##
+## Output: numeric; gives the estimator A2 for equal covariances
+make_C1_star_eq <- function(X, B, group){
+  ns <- unname(table(group))
+  a <- length(ns)
+  out <- 0.0
+  prefactor <- 0
+  for(i in 1:a){
+    n <- sum
+    out <- out + C1_eq(X[, group == i], B)
+  }
+  return(out/(8 * a * B)) ## 8 * a*B considering B is equal for all groups
 }
