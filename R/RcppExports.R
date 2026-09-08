0460a3d923d87d43e5a46224ac2b8e4c62d4ed79
Schnieboli <119417363+Schnieboli@users.noreply.github.com>
2026-09-08 15:49:36 +0200

    cpp functions renamed

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -45,8 +45,8 @@ C5star_cpp_internal <- function(X, group, B, n) {
     .Call(`_hdrm_C5star_cpp_internal`, X, group, B, n)
 }
 
-C5star_cpp_internal_list <- function(X_list, B) {
-    .Call(`_hdrm_C5star_cpp_internal_list`, X_list, B)
+C5star_cpp <- function(X_list, B) {
+    .Call(`_hdrm_C5star_cpp`, X_list, B)
 }
 
 B0_cpp <- function(mat) {
