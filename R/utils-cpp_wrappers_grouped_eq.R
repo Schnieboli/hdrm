a7ee5862568dc968d822b6dcbc39ddf5e0431b32
Schnieboli <119417363+Schnieboli@users.noreply.github.com>
2026-09-16 16:40:00 +0200

    typo

--- R/utils-cpp_wrappers_grouped_eq.R
+++ R/utils-cpp_wrappers_grouped_eq.R
@@ -2,7 +2,7 @@ A1_eq <- function(X_TS_list){
   n_i <- sapply(X_TS_list, ncol)
   A1_body <- sum(sapply(X_TS_list, A1_i_eq_cpp))
   A1 <- A1_body / sum(n_i*(n_i-1))
-  if (is.(A1) || !is.finite(A1) || A1 < 0)
+  if (is.na(A1) || !is.finite(A1) || A1 < 0)
     stop("The test is degenerate for these data.", call. = FALSE)
   A1
 }
@@ -12,7 +12,7 @@ A2_eq <- function(X_TS_list){
   n_i <- sapply(X_TS_list, ncol)
   A2_body <- sum(sapply(X_TS_list, A2_i_eq_cpp))
   A2 <- A2_body / (24 * sum(choose(n_i, 4)))
-  if (is.(A2) || !is.finite(A2) || A2 < 0)
+  if (is.na(A2) || !is.finite(A2) || A2 < 0)
     stop("The test is degenerate for these data.", call. = FALSE)
   A2
 }
@@ -26,7 +26,7 @@ C1star_eq <- function(X_TS_list, B){
     C1_body <- C1_body + C1star_i_eq_cpp(X_TS_list[[i]], B_i[i])
   }
   C1 <- C1_body / sum(8 * B_i)
-  if (is.(C1) || !is.finite(C1) || C1 < 0)
+  if (is.na(C1) || !is.finite(C1) || C1 < 0)
     stop("The test is degenerate for these data.", call. = FALSE)
   C1
 }
