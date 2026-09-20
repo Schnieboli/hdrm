1a20de799599d1fa1cf375363aeee672243173c6
Schnieboli <119417363+Schnieboli@users.noreply.github.com>
2026-09-20 12:58:07 +0200

    spacing

--- R/utils-compute_grouped_df.R
+++ R/utils-compute_grouped_df.R
@@ -1,21 +1,19 @@
 #' @keywords internal
-compute_grouped_df <- function(
-    second_order,
-    third_order,
-    design_factor = 1
-) {
+compute_grouped_df <- function(second_order,
+                               third_order,
+                               design_factor = 1) {
   quantities <- c(
     second_order = second_order,
     third_order = third_order,
     design_factor = design_factor
   )
   
-  if (
-    any(lengths(list(second_order, third_order, design_factor)) != 1L) ||
-    !is.numeric(quantities) ||
-    anyNA(quantities) ||
-    any(!is.finite(quantities))
-  ) {
+  if (any(lengths(list(
+    second_order, third_order, design_factor
+  )) != 1L) ||
+  !is.numeric(quantities) ||
+  anyNA(quantities) ||
+  any(!is.finite(quantities))) {
     stop(
       paste0(
         "The second-order trace estimate, third-order trace estimate, and ",
@@ -26,10 +24,7 @@ compute_grouped_df <- function(
   }
   
   if (second_order <= 0) {
-    stop(
-      "The second-order trace estimate must be positive.",
-      call. = FALSE
-    )
+    stop("The second-order trace estimate must be positive.", call. = FALSE)
   }
   
   if (third_order == 0) {
@@ -43,22 +38,15 @@ compute_grouped_df <- function(
   }
   
   if (design_factor <= 0) {
-    stop(
-      "The design factor must be positive.",
-      call. = FALSE
-    )
+    stop("The design factor must be positive.", call. = FALSE)
   }
   
-  degrees_of_freedom <- as.numeric(
-    (second_order^3 / third_order^2) * design_factor
-  )
+  degrees_of_freedom <- as.numeric((second_order^3 / third_order^2) * design_factor)
   
-  if (
-    length(degrees_of_freedom) != 1L ||
-    is.na(degrees_of_freedom) ||
-    !is.finite(degrees_of_freedom) ||
-    degrees_of_freedom <= 0
-  ) {
+  if (length(degrees_of_freedom) != 1L ||
+      is.na(degrees_of_freedom) ||
+      !is.finite(degrees_of_freedom) ||
+      degrees_of_freedom <= 0) {
     stop(
       paste0(
         "The estimated degrees-of-freedom parameter is not finite and ",
@@ -68,8 +56,5 @@ compute_grouped_df <- function(
     )
   }
   
-  max(
-    1,
-    degrees_of_freedom
-  )
+  max(1, degrees_of_freedom)
 }
