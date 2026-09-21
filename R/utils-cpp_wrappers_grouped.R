4d5fcb15834704d1c4f4f03c071cda966f4bb8a4
Schnieboli <119417363+Schnieboli@users.noreply.github.com>
2026-09-21 14:07:46 +0200

    merge files

--- R/utils-cpp_wrappers_grouped.R
+++ R/utils-cpp_wrappers_grouped.R
@@ -1,3 +1,6 @@
+
+# Unequal Covariance ------------------------------------------------------
+
 A1 <- function(X_TS_list, subsampling, B){
   a <- length(X_TS_list)
   if(subsampling){
@@ -11,6 +14,7 @@ A1 <- function(X_TS_list, subsampling, B){
   }
   A1
 }
+
 
 A4 <- function(X_TS_list, TW, subsampling, B){
   a <- length(X_TS_list)
@@ -65,3 +69,38 @@ C5star <- function(data_list, TW, TS, B){
   C5star_cpp(Y_list, B = joint_B)
 }
 
+
+# Equal covariance --------------------------------------------------------
+
+A1_eq <- function(X_TS_list){
+  n_i <- sapply(X_TS_list, ncol)
+  A1_body <- sum(sapply(X_TS_list, A1_i_eq_cpp))
+  A1 <- A1_body / sum(n_i*(n_i-1))
+  if (is.na(A1) || !is.finite(A1) || A1 < 0)
+    stop("The test is degenerate for these data.", call. = FALSE)
+  A1
+}
+
+
+A2_eq <- function(X_TS_list){
+  n_i <- sapply(X_TS_list, ncol)
+  A2_body <- sum(sapply(X_TS_list, A2_i_eq_cpp))
+  A2 <- A2_body / (24 * sum(choose(n_i, 4)))
+  if (is.na(A2) || !is.finite(A2) || A2 < 0)
+    stop("The test is degenerate for these data.", call. = FALSE)
+  A2
+}
+
+
+C1star_eq <- function(X_TS_list, B){
+  n_i <- sapply(X_TS_list, ncol)
+  B_i <- allocate_C1_subsamples(group_sizes = n_i, B = B)
+  C1_body <- 0
+  for(i in seq_along(X_TS_list)){
+    C1_body <- C1_body + C1star_i_eq_cpp(X_TS_list[[i]], B_i[i])
+  }
+  C1 <- C1_body / sum(8 * B_i)
+  if (is.na(C1) || !is.finite(C1) || C1 < 0)
+    stop("The test is degenerate for these data.", call. = FALSE)
+  C1
+}
